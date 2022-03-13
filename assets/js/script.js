350adc53568a6bb181366414979d8a0d3d56e748
olyvia lassiter <olyviajlassiter@gmail.com>
2022-03-12 18:30:34 -0600

    added time blocks, current date, and style elements

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    let NowMoment = moment().format("MMMM Do YYYY");
+    let displayDate = document.getElementById("currentDay");
+    displayDate.innerHTML = NowMoment;
+})
